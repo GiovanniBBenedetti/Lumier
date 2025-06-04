b01515a376b5bcf5d3e09cf9fc2ac3e86ba9c8dd
GiovanniBBendetti <senaigiovannibenedetti@gmail.com>
2025-06-04 16:51:24 -0300

    Fontes retiradas do layout.jsx

--- frontEnd/src/app/layout.jsx
+++ frontEnd/src/app/layout.jsx
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import Nav from "@/components/navbar/page";
 import Footer from "@/components/footer/page";
@@ -6,15 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -23,9 +14,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="pt-BR">
+
+      <body>
         <Nav></Nav>
         {children}
         <Footer></Footer>
